7e68e5de7d75a50ad4c516a412272f57413d86f9
AM Codes <amccodes@gmail.com>
2022-07-03 17:15:52 +0700

    Create app food market using phone gap

---
+++ www/js/foods.js
@@ -0,0 +1,34 @@
+var foods = {
+    items: [
+        {
+            "id": 1,
+            "picturePath": "https://i.pinimg.com/736x/06/7b/28/067b2879e5c9c42ec669bf639c3fbffc.jpg",
+            "name": "Sate Sayur Sultan",
+            "price": 150000,
+        },
+        {
+            "id": 2,
+            "picturePath": "https://cdns.klimg.com/dream.co.id/resources/news/2020/04/06/133546/bikin-steak-di-rumah-pastikan-bumbunya-meresap-2004066.jpg",
+            "name": "Steak Daging Sapi Korea",
+            "price": 750000,
+        },
+        {
+            "id": 3,
+            "picturePath": "https://i.pinimg.com/736x/06/7b/28/067b2879e5c9c42ec669bf639c3fbffc.jpg",
+            "name": "Mexican Chopped Salad",
+            "price": 800000,
+        },
+        {
+            "id": 4,
+            "picturePath": "https://images.immediate.co.uk/production/volatile/sites/2/2016/08/25097.jpg?quality=90&resize=768,574",
+            "name": "Sup Wortel Pedas",
+            "price": 750000,
+        },
+        {
+            "id": 5,
+            "picturePath": "https://i.pinimg.com/736x/06/7b/28/067b2879e5c9c42ec669bf639c3fbffc.jpg",
+            "name": "Korean Raw Beef Tartare",
+            "price": 200000,
+        }
+    ]
+}
